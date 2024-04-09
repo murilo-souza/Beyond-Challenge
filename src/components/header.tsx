71e2c3f6e2e4d27ed69c346ee0216a84b90742a7
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-04-09 14:58:27 -0300

    feat: set a default value on selectors

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,11 +5,11 @@ import { Selector } from './selector'
 export function Header() {
   return (
     <header className="flex items-center justify-between px-3 py-2 bg-header">
-      <img src={Logo} alt="Logo"  />
-      <div className='flex items-center gap-10'>
-        <Selector/>
-        <ArrowRight className='size-7 text-white'/>
-        <Selector/>
+      <img src={Logo} alt="Logo" />
+      <div className="flex items-center gap-10">
+        <Selector defaultValue={'SBSP'} />
+        <ArrowRight className="size-7 text-white" />
+        <Selector defaultValue={'SBRJ'} />
       </div>
     </header>
   )
--- src/components/selector.tsx
+++ src/components/selector.tsx
@@ -1,26 +1,25 @@
-import { SelectHTMLAttributes } from "react"
+import { SelectHTMLAttributes } from 'react'
 
 type SelectProps = SelectHTMLAttributes<HTMLSelectElement>
 
-
-export function Selector({...rest}: SelectProps) {
+export function Selector({ ...rest }: SelectProps) {
   return (
     <select
-    {...rest}
-    className="inline-flex items-center justify-center rounded-md py-0 px-3 text-sm h-10 bg-header text-zinc-200 border-2 border-solid border-white"
-  >
-    <option
-      className="text-sm text-zinc-200 rounded flex items-center h-6 py-0 pl-9 pr-6 relative select-none"
-      value="SBSP"
+      {...rest}
+      className="inline-flex items-center justify-center rounded-md py-0 px-3 text-sm h-10 bg-header text-zinc-200 border-2 border-solid border-white"
     >
-      Aeroporto de Congonhas
-    </option>
-    <option
-      className="text-sm text-zinc-200 rounded flex items-center h-6 py-0 pl-9 pr-6 relative select-none"
-      value="SBRJ"
-    >
-      Aeroporto Santos Dumont
-    </option>
-  </select>
+      <option
+        className="text-sm text-zinc-200 rounded flex items-center h-6 py-0 pl-9 pr-6 relative select-none"
+        value="SBSP"
+      >
+        Aeroporto de Congonhas
+      </option>
+      <option
+        className="text-sm text-zinc-200 rounded flex items-center h-6 py-0 pl-9 pr-6 relative select-none"
+        value="SBRJ"
+      >
+        Aeroporto Santos Dumont
+      </option>
+    </select>
   )
 }
