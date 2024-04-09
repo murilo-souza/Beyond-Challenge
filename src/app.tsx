26a8fcb142b1138b1c21e13e392a60f229217c8d
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-04-09 12:48:36 -0300

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import { Header } from "./components/header";
+
+export function App() {
+  return (
+    <Header/>
+  )
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        header: '#141414',
+        modal: '#27292E'
+      }
+    },
+  },
+  plugins: [],
+}
