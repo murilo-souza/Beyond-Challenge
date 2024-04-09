d60f7f6c9e64ca22fb34f45dae22c8ce2f5550ad
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-04-09 14:58:46 -0300

    feat: add map with the points and the polyline

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,73 @@
-import { Header } from "./components/header";
+import { MapPin } from 'lucide-react'
+import { Header } from './components/header'
+import ReactMapboxGL, {
+  GeolocateControl,
+  Layer,
+  Marker,
+  NavigationControl,
+  Source,
+} from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 export function App() {
+  const points = [
+    { latitude: -23.628221, longitude: -46.6596195 },
+    { latitude: -22.9102755, longitude: -43.1670474 },
+  ]
+
+  const polyline = {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+      type: 'LineString',
+      coordinates: points.map((point) => [point.longitude, point.latitude]),
+    },
+  }
+
   return (
-    <Header/>
+    <main>
+      <Header />
+      <div>
+        <ReactMapboxGL
+          mapboxAccessToken="pk.eyJ1IjoibXVyaWxvMzIzIiwiYSI6ImNsdXNsaDUwbDAyODMybW15YWpqcjhuenoifQ.Hzq9779iQ8n65zNh2a7Cew"
+          initialViewState={{
+            latitude: -23.628221,
+            longitude: -46.6596195,
+            zoom: 10,
+          }}
+          style={{ width: '100%', height: '90vh' }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        >
+          <div style={{ position: 'absolute', right: 10, top: 10 }}>
+            <NavigationControl />
+          </div>
+
+          <div style={{ position: 'absolute', right: 10, top: 40 }}>
+            <GeolocateControl />
+          </div>
+
+          {points.map((point, key) => (
+            <Marker
+              key={key}
+              longitude={point.longitude}
+              latitude={point.latitude}
+            >
+              <MapPin className="text-marker size-7" />
+            </Marker>
+          ))}
+
+          <Source type="geojson" data={polyline}>
+            <Layer
+              id="lineLayer"
+              type="line"
+              paint={{
+                'line-color': '#77BC58',
+                'line-width': 2,
+              }}
+            />
+          </Source>
+        </ReactMapboxGL>
+      </div>
+    </main>
   )
 }
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
         header: '#141414',
-        modal: '#27292E'
-      }
+        modal: '#27292E',
+        marker: '#77BC58',
+      },
     },
   },
   plugins: [],
